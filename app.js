900650b0fc069cbd85dd80a5121de4e631cea7cf
Kirtesh Patel <kirteshpatel@Kirteshs-MacBook-Pro.local>
2021-09-14 18:39:50 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const connectDB = require("./config/db");
+const methodOverride = require("method-override");
+// const MongoDBStore = require("connect-mongo")(session);
+const MongoDbStore = require("connect-mongo");
+
+// load config
+dotenv.config({ path: "./config/config.env" });
+
+// passport config
+require("./config/passport")(passport);
+
+// mongoose connection
+connectDB();
+
+const app = express();
+
+// body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
+// logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// handlebars helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
+
+// handlebars
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+
+// sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoDbStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// set global variables
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
+// static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/blogs", require("./routes/blogs"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
